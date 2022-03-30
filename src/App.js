917ec65a5ae2f878bb4a3d97fe15cf05edd71d51
rentianzhu <rentianzhu@bytedance.com>
2022-03-30 17:27:23 +0800

    notification

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {ChatEngine} from 'react-chat-engine';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export function App() {
+    requestNotificationPermission()
+    return (<ChatEngine
+        height='100vh'
+        userName='z'
+        userSecret='12345678'
+        projectID='cb14cd06-00be-4cc3-b17d-b8ef6bb2927f'
+        onNewMessage={(chatId, message) => {
+            new Notification(message.sender_username, {body: message.text}); // show a system notification
+        }}
+    />);
+}
+
+function requestNotificationPermission() {
+    Notification.requestPermission().then(r => console.log(r))
 }
 
 export default App;
